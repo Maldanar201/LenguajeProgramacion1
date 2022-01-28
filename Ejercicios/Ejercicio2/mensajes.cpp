2d6b5e6cb93af76016129ca0df4070237575ad28
Manuel Aldana <manuel.aldana@unah.hn>
2022-01-28 16:40:04 -0600

    agregando ejercicio2

---
+++ Ejercicios/Ejercicio2/mensajes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string nombre;
+
+    cout << " ingrese su nombre:  ";
+    cin >> nombre;
+
+    cout <<"\n Hola mi nombre es " << nombre;
+    
+    return 0;
+}
