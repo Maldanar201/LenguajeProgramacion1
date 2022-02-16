be054eb34412fafd14839a32e42c031155f2ca7c
Maldanar201 <98437765+Maldanar201@users.noreply.github.com>
2022-02-15 19:11:18 -0600

    add ejercicio 22

---
+++ Ejercicios/Ejercicio22-ciclosFor/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	
+	//declaracion de variables
+	int indice = 1;
+	
+	//Desarrollo 
+	for (indice = 1; indice <= 10; indice++)
+	{
+		cout <<" Valores de la variable indice "<< indice << endl;	
+	}
+	return 0;
+}
