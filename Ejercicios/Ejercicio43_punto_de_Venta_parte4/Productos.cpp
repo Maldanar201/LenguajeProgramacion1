8d64ce00a884c9d23d0fd4a0dcd523e5b420e96f
Maldanar201 <98437765+Maldanar201@users.noreply.github.com>
2022-03-06 16:52:33 -0600

    Add ejercicio43

---
+++ Ejercicios/Ejercicio43_punto_de_Venta_parte4/Productos.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+extern void agregarProducto(string descripcion, int cantidad, double precio);
+
+void Productos(int opcion)
+{
+		
+	system("cls");
+	int opcionProductos = 0;
+	
+	switch(opcion)
+	{
+	case 1:
+		{
+			cout <<" ### BEBIDAS CALIENTES ### "<< endl;
+			cout <<"==========================="<< endl;
+			cout << endl;
+			cout << " 1 - Capuccino "<<endl;
+			cout << " 2 - expresso "<<endl;
+			
+			cin >> opcionProductos;
+			
+			switch(opcionProductos)
+			{
+				
+				case 1:
+				{
+					agregarProducto("Capuccino", 1, 40);
+					break;
+				}
+				case 2:
+				{
+					agregarProducto("Expresso", 1, 30);
+					break;
+				}
+				default:
+				{
+					cout <<" Opcion no Valida ";
+					return;
+					break;
+				}
+				cout <<endl;
+				cout << "\n Producto agregado "<< endl;
+				system("pause");
+			}
+			
+		break;
+			
+		}
+	case 2:
+		{
+			cout <<" ### BEBIDAS FRIAS ### "<< endl;
+			cout <<"======================="<< endl;
+				
+			system("pause");
+		}
+	case 3:
+		{
+			cout <<" ### REPOSTERIA ### "<< endl;
+			cout <<"===================="<< endl;
+				
+			system("pause");
+		}
+	default:
+		break;
+	}
+}
---
+++ Ejercicios/Ejercicio43_punto_de_Venta_parte4/menu.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+extern void Productos(int opcion);
+extern void imprimirFactura();
+void menu()
+{
+	int opcion = 0;
+	
+	while(true)
+	{
+		system("cls");
+		cout << "     ************"<<endl;
+		cout << "     ****MENU****"<<endl;
+		cout << "     ************"<<endl;
+		cout <<endl;
+		cout <<endl;
+		
+		cout << " 1 - Bebidas Calientes "<<endl;
+		cout << " 2 - Bebidas Frias "<<endl;
+		cout << " 3 - Postres "<<endl;
+		cout << " 4 - Imprimir Factura "<<endl;
+		cout << " 0 - Salir "<<endl;
+		
+		cin >> opcion;
+		
+		if(opcion == 0)
+		{
+			break;
+		}
+		if(opcion == 4)
+		{
+			imprimirFactura();
+		}
+		else
+		{
+			Productos(opcion);
+		}
+		
+	}	
+}
