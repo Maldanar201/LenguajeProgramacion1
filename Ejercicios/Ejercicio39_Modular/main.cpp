fb65c94c2a36b53ba4cb2ef69b03258c52ee2e92
Maldanar201 <98437765+Maldanar201@users.noreply.github.com>
2022-03-06 12:38:02 -0600

    Add Ejercicio39

---
+++ Ejercicios/Ejercicio39_Modular/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+extern int suma(int a, int b);
+
+int main(int argc, char** argv) 
+{
+	system("cls");
+	
+	cout << suma(5,7)<<endl;
+	
+	return 0;
+}
