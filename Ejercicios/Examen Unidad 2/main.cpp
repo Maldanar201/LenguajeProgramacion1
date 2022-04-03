f9331977f0abe35bd9083bd78f2e008d89cac597
Maldanar201 <98437765+Maldanar201@users.noreply.github.com>
2022-04-03 15:35:05 -0600

    Add examen Unidad 2

---
+++ Ejercicios/Examen Unidad 2/main.cpp
@@ -0,0 +1,53 @@
+/* Examen Unidad 2 Lenguaje de Programacion I
+Elaborado: Manuel Aldana Rivas
+Propósito: Evaluacion de aprendizaje Aprendizaje 
+Fecha: 3 Abril de 2022
+Hora: 19:00
+*/
+
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+#include"Juego.h"
+
+using namespace std;
+
+#define ARRIBA 72
+#define IZQUIERDA 75
+#define ABAJO 80
+#define DERECHA 77
+#define ESC 27
+
+
+
+int main()
+{
+	
+	
+	char tecla;
+	int puntos=0;
+	int xPos=30, yPos=20;
+	
+	inicializarArreglo();
+	dificultad();
+	gotoxy(50,2);
+	cout << puntos;
+	pintar();
+	gotoxy(xPos, yPos);cout<<(char)4;
+	
+	while(tecla != ESC && gameover() )
+	{
+		proceso(tecla, puntos);
+	}
+	
+	if ( !gameover())
+	{
+		MessageBox(NULL,"Has perdido","Perdedor", MB_OK);
+		system("cls");
+	}
+	
+	system("pause>NULL");
+	return 0;
+}
